d159162dd93b836e58a252adb390cca35545505a
Sushant <Sushant>
2017-03-12 08:59:18 +0530

    added code

---
+++ app/common/commonHeader.js
@@ -0,0 +1,11 @@
+(function(){
+	
+	function cmHeaderFn(){
+		return{
+		templateUrl:'app/header/header.tpl.html',
+		restrict:"A,E,C"	
+	}
+ }
+	angular.module("common")
+	.directive("cmHeader",[cmHeaderFn]);
+})();
